95101726a6a8d7675b787b6a979f5313a991d32b
Thomas Constantine Moore <tomismore@gmail.com>
2024-08-26 17:51:23 -0400

    add placeholder Receipt class, accept it and echo back the invoice number

--- src/main/java/org/versa/sender/ServiceInfoController.java
+++ src/main/java/org/versa/sender/ServiceInfoController.java
@@ -1,8 +1,12 @@
 package org.versa.sender;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ServiceInfoController {
@@ -18,4 +22,22 @@ public class ServiceInfoController {
 		return "<div>Hello from the " + applicationName + "@" + versaSenderVersion + " service.</div><div>The registry url is " + versaRegistryUrl +"</div>";
 	}
 
+	@GetMapping("/registry")
+	public String registry() {
+		return checkRegistry();
+	}
+
+	@PostMapping("/send") 
+	public String send(@RequestBody Receipt newReceipt) { 
+		// TODO: accept transaction handles as well and regist the receipt
+		return newReceipt.getInvoiceNumber();
+	}
+
+	private String checkRegistry()
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(versaRegistryUrl, String.class);
+
+		return result;
+	}
 }
