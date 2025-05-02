100115530b105d0e6c214af4f614f8ec851268df
Thomas Moore <thomas@versa.org>
2025-05-02 15:17:17 -0400

    update example for schema_version 1.11.0

--- src/main/java/org/versa/sender/ReceiptRegistrationResponse.java
+++ src/main/java/org/versa/sender/ReceiptRegistrationResponse.java
@@ -3,12 +3,11 @@ package org.versa.sender;
 import java.util.List;
 
 public class ReceiptRegistrationResponse {
-  public String env;
   public VersaMode mode;
-  public String event_id;
   public String receipt_id;
   public String transaction_id;
   public List<Receiver> receivers;
   public String encryption_key;
   /// DEPRECATED: use 'mode' instead
+  public String env;
 }
