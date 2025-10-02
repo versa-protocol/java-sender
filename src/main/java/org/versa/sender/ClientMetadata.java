a429c5a25b6b11d6d8ae033ebe79091073a3f728
thomas <thomas@versa.org>
2025-10-02 11:17:00 -0400

    add client_metadata to example ReceiptRegistrationRequest

---
+++ src/main/java/org/versa/sender/ClientMetadata.java
@@ -0,0 +1,9 @@
+package org.versa.sender;
+
+public class ClientMetadata {
+  public String client_string;
+
+  public ClientMetadata(String client_string) {
+    this.client_string = client_string;
+  }
+}
--- src/main/java/org/versa/sender/ReceiptRegistrationRequest.java
+++ src/main/java/org/versa/sender/ReceiptRegistrationRequest.java
@@ -4,10 +4,12 @@ public class ReceiptRegistrationRequest {
   public String schema_version;
   public TransactionHandles handles;
   public String transaction_id;
+  public ClientMetadata client_metadata;
 
   public ReceiptRegistrationRequest(String schema_version, TransactionHandles handles, String transaction_id) {
     this.schema_version = schema_version;
     this.handles = handles;
     this.transaction_id = transaction_id;
+    this.client_metadata = new ClientMetadata("java-sender/1.0.0");
   }
 }
